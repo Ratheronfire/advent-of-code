bbbf9924da82aedba2817768aaf799808060763c
Ratheronfire <ratheronfire@gmail.com>
2023-11-08 01:25:59 -0500

    2015 days 8 and 9.

--- 2015/day-8.py
+++ 2015/day-8.py
@@ -2,25 +2,66 @@ from typing import List
 
 from puzzle_base import PuzzleBase
 
+HEX_CHARS = '0123456789abcdefABCDEF'
+
 
 class Puzzle(PuzzleBase):
     year = 2015
     day = 8
 
-    sample_data = 0
+    lines: list[str]
 
     def reset(self):
-        self.sample_data = 0
+        self.lines = []
 
     def prepare_data(self, input_data: List[str], current_part: int):
-        for i in range(len(input_data)):
-            line = input_data[i]
+        self.lines = [l for l in input_data if len(l) > 0]
+
+    def len_escaped(self, line: str) -> int:
+        count = 0
+
+        i = 1
+        while i < len(line) - 1:
+            char = line[i]
+
+            if char == '\\':
+                if line[i+1] in '\\"':
+                    i += 1
+                elif line[i+1] == 'x' and line[i+2] in HEX_CHARS and line[i+3] in HEX_CHARS:
+                    i += 3
+
+            i += 1
+            count += 1
+
+        print(f'{line} - {len(line)} characters, {count} escaped characters')
+        return count
+
+    def escape_line(self, line: str) -> str:
+        out_str = '"'
+
+        i = 0
+        while i < len(line):
+            char = line[i]
+
+            if char == '"':
+                out_str += '\\"'
+            elif char == '\\':
+                out_str += '\\\\'
+            else:
+                out_str += char
+
+            i += 1
+
+        out_str += '"'
+        print(f'{line} -> {out_str} ({len(line)} -> {len(out_str)})')
+
+        return out_str
 
     def get_day_1_answer(self, use_sample=False) -> str:
-        return ''
+        return str(sum([len(l) - self.len_escaped(l) for l in self.lines]))
 
     def get_day_2_answer(self, use_sample=False) -> str:
-        return ''
+        return str(sum([len(self.escape_line(l)) - len(l) for l in self.lines]))
 
 
 if __name__ == "__main__":
--- 2015/day-9.py
+++ 2015/day-9.py
@@ -3,24 +3,83 @@ from typing import List
 from puzzle_base import PuzzleBase
 
 
+class City:
+    id: str
+
+    def __init__(self, id: str):
+        self.id = id
+
+
 class Puzzle(PuzzleBase):
     year = 2015
     day = 9
 
-    sample_data = 0
+    cities = dict[str, City]
+    city_paths = {}
 
     def reset(self):
-        self.sample_data = 0
+        self.cities = {}
+        self.city_paths = {}
 
     def prepare_data(self, input_data: List[str], current_part: int):
         for i in range(len(input_data)):
             line = input_data[i]
 
+            if line == '':
+                continue
+
+            parts = line.split(' ')
+            if parts[0] not in self.cities:
+                self.cities[parts[0]] = City(parts[0])
+            if parts[2] not in self.cities:
+                self.cities[parts[2]] = City(parts[2])
+
+            self.city_paths[(parts[0], parts[2])] = int(parts[4])
+            self.city_paths[(parts[2], parts[0])] = int(parts[4]) # getting the reverse too
+
+    def get_permutations(self, list: List) -> List:
+        if len(list) < 2:
+            return list
+
+        if len(list) == 2:
+            return [[list[0], list[1]], [list[1], list[0]]]
+
+        permutations = []
+        for elem in list:
+            sublist = [e for e in list if e != elem]
+            sub_permutations = self.get_permutations(sublist)
+
+            for sub_permutation in sub_permutations:
+                if [elem] + sub_permutation not in permutations:
+                    permutations.append([elem] + sub_permutation)
+                if sub_permutation + [elem] not in permutations:
+                    permutations.append(sub_permutation + [elem])
+
+        return permutations
+
+    def get_distance(self, path: list[str]):
+        total = 0
+
+        for i in range(len(path) - 1):
+            connection = (path[i], path[i + 1])
+            if connection not in self.city_paths:
+                return -1  # this path is impossible
+
+            total += self.city_paths[connection]
+
+        return total
+
     def get_day_1_answer(self, use_sample=False) -> str:
-        return ''
+        permutations = self.get_permutations([c.id for c in self.cities.values()])
+        distances = [self.get_distance(p) for p in permutations]
+
+        return str(min([d for d in distances if d >= 0]))
 
     def get_day_2_answer(self, use_sample=False) -> str:
-        return ''
+        permutations = self.get_permutations([c.id for c in self.cities.values()])
+        distances = [self.get_distance(p) for p in permutations]
+
+        return str(max([d for d in distances if d >= 0]))
 
 
 if __name__ == "__main__":
