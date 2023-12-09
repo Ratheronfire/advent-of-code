cf40ee1a7fc259bd8e59e8ff69038f7900bb0e4a
Ratheronfire <ratheronfire@gmail.com>
2023-12-09 00:45:49 -0500

    2023-9.

--- 2023/day-9.py
+++ 2023/day-9.py
@@ -7,20 +7,46 @@ class Puzzle(PuzzleBase):
     year = 2023
     day = 9
 
-    sample_data = 0
+    value_sets: list[list[int]]
 
     def reset(self):
-        self.sample_data = 0
+        self.value_sets = []
 
     def prepare_data(self, input_data: List[str], current_part: int):
         for i in range(len(input_data)):
             line = input_data[i]
+            if line != '':
+                self.value_sets.append([int(i) for i in line.split()])
+
+    def get_next_value(self, value_set: list[int], forwards=True):
+        edge_values = [value_set[-1 if forwards else 0]]
+
+        derivative_values = []
+
+        while not all([v == 0 for v in value_set]):
+            for i in range(len(value_set) - 1):
+                derivative_values.append(value_set[i + 1] - value_set[i])
+
+            value_set = derivative_values.copy()
+            derivative_values = []
+
+            edge_values.append(value_set[-1 if forwards else 0])
+
+        if forwards:
+            return sum(edge_values)
+        else:
+            num = 0
+            edge_values.reverse()
+            for edge_value in edge_values:
+                num = edge_value - num
+
+            return num
 
     def get_part_1_answer(self, use_sample=False) -> str:
-        return ''
+        return str(sum([self.get_next_value(v, True) for v in self.value_sets]))
 
     def get_part_2_answer(self, use_sample=False) -> str:
-        return ''
+        return str(sum([self.get_next_value(v, False) for v in self.value_sets]))
 
 
 if __name__ == "__main__":
