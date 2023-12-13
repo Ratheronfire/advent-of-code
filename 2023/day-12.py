87d5d1176e6ea796cd2b77f78922039687ae66c7
Ratheronfire <ratheronfire@gmail.com>
2023-12-13 01:10:39 -0500

    Closer on 2023.12.2.

--- 2023/day-12.py
+++ 2023/day-12.py
@@ -10,7 +10,7 @@ class GearRow:
         self.runs = runs
 
     def __str__(self):
-        return f'{self.gears} {self.runs}'
+        return f'{"".join(self.gears)} {",".join([str(r) for r in self.runs])}'
 
     def get_row_margin(self):
         return len(self.gears) - sum(self.runs) - (len(self.runs) - 1)
@@ -87,7 +87,7 @@ class GearRow:
         self.gears = simplified_gears[1:]
         self.runs = run_lengths
 
-    def is_valid(self) -> bool:
+    def is_valid(self, fail_if_more_broken_gears=True) -> bool:
         current_run = 0
 
         gears = self.gears.copy()
@@ -109,7 +109,7 @@ class GearRow:
                 runs_to_test = runs_to_test[1:]
 
                 if len(runs_to_test) == 0:
-                    return '#' not in gears[i+1:]
+                    return '#' not in gears[i+1:] or not fail_if_more_broken_gears
 
         return False
 
@@ -136,25 +136,57 @@ class Puzzle(PuzzleBase):
                 [int(r) for r in broken_runs.split(',')]
             ))
 
-    def get_working_permutations(self, gear_row: GearRow):
-        if not len(gear_row.runs):
-            return 1  # this row is already satisfied
-
-        if '?' not in gear_row.gears:
-            # print(''.join(gear_row.gears), gear_row.runs, f' - Is Valid: {self.is_row_valid(gear_row)}')
+    def get_working_permutations(self, gear_row: GearRow, runs_filled=[]):
+        if len(gear_row.runs) and '?' not in gear_row.gears:
+            return 0  # no more spots to fill, but not all runs are satisfied
+        elif not len(gear_row.runs):
+            return 0 if '?' in gear_row.gears else 1  # this row is already satisfied
+        elif '?' not in gear_row.gears:
             return int(gear_row.is_valid())
-        else:
-            next_unknown = gear_row.gears.index('?')
 
-            broken_row = gear_row.gears.copy()
-            broken_row[next_unknown] = '#'
-            is_broken_valid = self.get_working_permutations(GearRow(broken_row, gear_row.runs))
+        gears = list(gear_row.gears).copy()
+        gears.append('.')
 
-            working_row = gear_row.gears.copy()
-            working_row[next_unknown] = '.'
-            is_working_valid = self.get_working_permutations(GearRow(working_row, gear_row.runs))
+        first_unknown = gears.index('?')
+        prior_row = GearRow(gears[:first_unknown], runs_filled)
+        if len(runs_filled) and not prior_row.is_valid(False):
+            return 0  # this row is invalidated
 
-            return int(is_broken_valid) + int(is_working_valid)
+        candidate_gear_rows = set()
+        next_runs = gear_row.runs.copy()[1:]
+        space_to_fill = gear_row.runs[0]
+
+        breaks_seen = 0
+
+        # print(f'Testing row: {gear_row}')
+
+        for i in range(len(gears) - space_to_fill):
+            if i > first_unknown and gears[i] == '#':
+                breaks_seen += 1
+
+            if breaks_seen > space_to_fill:
+                break  # we're past the point this run can fit in, so no sense in continuing
+
+            if (1 > 0 and gears[i-1] == '#') or gears[i+space_to_fill] == '#' or \
+                    not all([gear in '#?' for gear in gears[i:i+space_to_fill]]):
+                # can only claim this space if it's all broken or unknown, and the borders are not #
+                continue
+
+            new_gears = gears.copy()
+
+            for j in range(0, i + space_to_fill + 1):
+                if new_gears[j] == '?':
+                    new_gears[j] = '.'
+
+            for j in range(space_to_fill):
+                new_gears[i + j] = '#'
+            new_gears[i + space_to_fill] = '.'
+
+            candidate_gear_rows.add(tuple(new_gears[:-1]))
+
+        return sum(
+            [self.get_working_permutations(GearRow(c, next_runs), runs_filled + [gear_row.runs[0]])
+             for c in candidate_gear_rows])
 
     def expand_rows(self):
         new_rows = []
@@ -191,8 +223,6 @@ class Puzzle(PuzzleBase):
         return str(self.calculate_solutions())
 
     def get_part_2_answer(self, use_sample=False) -> str:
-        return ''
-
         self.expand_rows()
         return str(self.calculate_solutions())
 
