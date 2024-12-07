0effca9344a25153d4b5d4c00572991ef8dbd992
Ratheronfire <ratheronfire@gmail.com>
2024-12-07 11:44:18 -0500

    2024.3.

--- year_2024/day-3.py
+++ year_2024/day-3.py
@@ -1,26 +1,60 @@
+import functools
+import re
 from typing import List
 
 from puzzle_base import PuzzleBase
+
+
+class Function:
+    command: str
+    variables: list
+
+    def __init__(self, command: str, variables: list):
+        self.command = command
+        self.variables = variables
+
+    def run(self):
+        if self.command == "mul":
+            return functools.reduce(lambda a, b: a*b, [int(v) for v in self.variables])
 
 
 class Puzzle(PuzzleBase):
     year = 2024
     day = 3
 
-    sample_data = 0
+    program_memory = ""
 
     def reset(self):
-        self.sample_data = 0
+        self.program_memory = ""
 
     def prepare_data(self, input_data: List[str], current_part: int):
-        for i in range(len(input_data)):
-            line = input_data[i]
+        self.program_memory = "\n".join(input_data)
+
+    def get_matching_functions(self, function_names: list[str]) -> list[Function]:
+        regex = r"(%s)\(((\d+), ?(\d+))?\)" % "|".join(function_names)
+        pattern = re.compile(regex)
+
+        matches = pattern.findall(self.program_memory)
+        return [Function(m[0], m[2:]) for m in matches]
 
     def get_part_1_answer(self, use_sample=False) -> str:
-        return ''
+        functions = self.get_matching_functions(["mul"])
+        return str(sum([f.run() for f in functions]))
 
     def get_part_2_answer(self, use_sample=False) -> str:
-        return ''
+        functions = self.get_matching_functions(["mul", "do", "don't"])
+
+        sum = 0
+        active = True
+        for function in functions:
+            if function.command == "do":
+                active = True
+            elif function.command == "don't":
+                active = False
+            elif function.command == "mul" and active:
+                sum += function.run()
+
+        return str(sum)
 
 
 if __name__ == "__main__":
